c030b087a8c652b6a9ff11784645cd68903d5272
Simon Wex <simon@simonwex.com>
2014-06-11 19:15:06 -0700

    Handling external resources... the super gross process script lives on\!

--- process.sh
+++ process.sh
@@ -8,23 +8,39 @@ cp index.html.1 index.html
 for COMPONENT in $(ls -l | egrep '^d' | grep 'component' | sed 's/.*component/component/')
 do
   echo $COMPONENT
-  curl http://mozilla-appmaker.github.io/$COMPONENT/component.html > $COMPONENT/component.html 2> /dev/null
-  curl -f http://mozilla-appmaker.github.io/$COMPONENT/component.css > $COMPONENT/component.css 2> /dev/null
-  mkdir thumbnails/$COMPONENT/
-  curl -f http://mozilla-appmaker.github.io/$COMPONENT/thumbnail.png > thumbnails/$COMPONENT/thumbnail.png 2> /dev/null
-  # curl http://mozilla-appmaker.github.io/$COMPONENT/thumbnail.png > thumbnails/$COMPONENT.png 2> /dev/null
-  # sed "s/thumbnail\.png/$COMPONENT.png/" $COMPONENT/component.html > ./tmpfile.html
-  # mv tmpfile.html $COMPONENT/component.html
+  wget -O $COMPONENT.zip https://github.com/mozilla-appmaker/$COMPONENT/archive/gh-pages.zip
+  rm -rf $COMPONENT
+  unzip $COMPONENT.zip && mv $COMPONENT-gh-pages $COMPONENT
+  rm $COMPONENT.zip
   echo "<link rel=\"import\" href=\"$COMPONENT/component.html\">"  >> index.html
+
+  # Copy assets to bundles
+
+  rm -rf $COMPONENT/conf
+  rm -rf $COMPONENT/tools
+  rm -rf $COMPONENT/test
+  rm -rf $COMPONENT/template
+
+
+
+  for ASSET_PATH in `ls -d $COMPONENT/*/`
+  do
+    mkdir -p ~/projects/mozilla-appmaker/appmaker/public/bundles/components/$ASSET_PATH
+    cp -R $ASSET_PATH/* ~/projects/mozilla-appmaker/appmaker/public/bundles/components/$ASSET_PATH/
+  done
+
+
+
+  cp $COMPONENT/thumbnail.png ~/projects/mozilla-appmaker/appmaker/public/bundles/components/$COMPONENT/thumbnail.png
 done
+
 
 cat index.html.2 >> index.html
 
 vulcanize index.html
 
 mkdir -p ~/projects/mozilla-appmaker/appmaker/public/bundles/components
-rm -rf ~/projects/mozilla-appmaker/appmaker/public/bundles/components/*
-mv thumbnails/* ~/projects/mozilla-appmaker/appmaker/public/bundles/components/
+
 cp vulcanized.html ~/projects/mozilla-appmaker/appmaker/public/bundles/components/mozilla-appmaker.html
 
 for FILE_PATH in "component-camera-button/images/arrow-up.png" "component-camera-button/images/camera-asleep.png" "component-camera-button/images/folder-photo.png"
