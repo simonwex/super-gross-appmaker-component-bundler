f521b2b9a22d710af7119aa9ac49b5c20d38af45
Simon Wex <simon@simonwex.com>
2014-06-24 10:31:07 -0700

    Updating component list

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-mkdir thumbnails
-
+mkdir components
 mkdir component-addform
 mkdir component-alternating-gate
 mkdir component-audio
@@ -9,27 +8,36 @@ mkdir component-boolean
 mkdir component-button
 mkdir component-button-with-confirmation
 mkdir component-camera-button
+mkdir component-canvas
+mkdir component-chart
 mkdir component-chat-window
+mkdir component-combiner
 mkdir component-counter
+mkdir component-cowbell
+mkdir component-daily-counter
 mkdir component-dataview
 mkdir component-editform
 mkdir component-fireworks
 mkdir component-header
 mkdir component-hot-potato
 mkdir component-image
+mkdir component-jazzhands
+mkdir component-kickdrum
+mkdir component-meatspaces-input
+mkdir component-meatspaces-messages
 mkdir component-metronome
 mkdir component-microphone-button
 mkdir component-paypal
 mkdir component-random-number
+mkdir component-sequencer
+mkdir component-snaredrum
 mkdir component-spacer
 mkdir component-splitter
 mkdir component-text-input
 mkdir component-textbox
 mkdir component-transformer
-mkdir component-kickdrum
-mkdir component-sequencer
-mkdir component-cowbell
-mkdir component-snaredrum
-mkdir component-meatspaces-messages
-mkdir component-meatspaces-input
-mkdir component-canvas
+mkdir component-pad-grid
+mkdir component-notebook
+mkdir component-channel-gate
+mkdir component-double-button
+mkdir component-todo
