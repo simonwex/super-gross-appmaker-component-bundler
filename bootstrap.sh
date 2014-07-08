2bf9f2bc7405e638724a6bf274cf29d8ae656ace
Bobby Richter <secretrobotron@gmail.com>
2014-07-08 14:37:08 -0400

    Update bootstrap.sh
    Added dataarray and media components.

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,7 @@ mkdir component-combiner
 mkdir component-counter
 mkdir component-cowbell
 mkdir component-daily-counter
+mkdir component-dataarray
 mkdir component-dataview
 mkdir component-editform
 mkdir component-fireworks
@@ -25,6 +26,7 @@ mkdir component-jazzhands
 mkdir component-kickdrum
 mkdir component-meatspaces-input
 mkdir component-meatspaces-messages
+mkdir component-media
 mkdir component-metronome
 mkdir component-microphone-button
 mkdir component-paypal
